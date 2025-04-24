43cd66bd350b6d20746e6aa1a86153cd8be3d501
rizwan8015 <Rizwanriz628@gmail.com>
2025-04-24 12:36:07 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sheik project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sheik.settings')
+
+application = get_wsgi_application()
